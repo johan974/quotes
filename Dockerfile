6b74d5edac184222e1e27723109a2d3c1db8b07d
johan974 <jj974@upcmail.nl>
2023-10-16 18:09:58 +0200

    Ported to JDK17.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM java:8-jdk-alpine
+FROM openjdk:17-alpine
 COPY ./target/quotes-0.0.1-SNAPSHOT.jar /usr/application/
+EXPOSE 8080
 WORKDIR /usr/application
 ENTRYPOINT ["java","-jar","quotes-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/lct/tutorial/quotes/QuotesApplication.java
+++ src/main/java/com/lct/tutorial/quotes/QuotesApplication.java
@@ -8,15 +8,18 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.request.async.DeferredResult;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class QuotesApplication {
+	private static final String QuoteUrl = "https://api.forismatic.com";
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuotesApplication.class, args);
@@ -26,29 +29,30 @@ public class QuotesApplication {
 	@RequestMapping("/random-quote")
 	class QuoteResource{
 		@Autowired
-		RestTemplate restTemplate;
+		private WebClient webClient;
 
 
 		@GetMapping
-		DeferredResult<String> getQuote(){
-			DeferredResult result = new DeferredResult();
-			final HttpHeaders headers = new HttpHeaders();
-			headers.set("User-Agent", "PostmanRuntime/7.15.2");
-			headers.set("Accept", "*/*");
+		Mono<QuoteResponse> getQuote(){
+//			final HttpHeaders headers = new HttpHeaders();
+//			headers.set("User-Agent", "PostmanRuntime/7.15.2");
+//			headers.set("Accept", "*/*");
 
-			final HttpEntity<String> entity = new HttpEntity<String>(headers);
-			QuoteResponse response = restTemplate.postForObject("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en",entity, QuoteResponse.class);
-			result.setResult(response.getQuoteText());
-			return result;
+			return webClient.get()
+					.uri("/api/1.0/?method=getQuote&format=json&lang=en")
+					.retrieve()
+					.bodyToMono(QuoteResponse.class);
 		}
 	}
 
-
-
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
+	public WebClient webClient() {
+		return WebClient.builder()
+				.baseUrl(QuoteUrl)
+				.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+				.build();
 	}
+
 
 }
 @JsonIgnoreProperties(ignoreUnknown = true)
